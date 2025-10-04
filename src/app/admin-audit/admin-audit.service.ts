d2c0eb1d40a2be08d1aacc805afb20229c3e79c7
poojagupta20 <poojagupta22042003@gmail.com>
2025-10-04 22:47:45 +0530

    updated code

--- src/app/admin-audit/admin-audit.service.ts
+++ src/app/admin-audit/admin-audit.service.ts
@@ -20,3 +20,32 @@ export class AdminAuditService {
     return this.http.get<any[]>(`${CURRENT_API_URL}/server/api/audit/user`, { params });
   }
 }
+
+
+// import { HttpClient, HttpParams } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { Observable } from 'rxjs';
+// import { CURRENT_API_URL } from '../core/serachpage/api-urls';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class AdminAuditService {
+//   private apiUrl = '/api/audit';
+
+//   constructor(private http: HttpClient) {}
+
+//   getAllUsers(): Observable<any> {
+//     return this.http.get<any[]>(`${CURRENT_API_URL}/server/api/audit/users`);
+//   }
+
+//   getAuditLogsByUser(userId: string): Observable<any> {
+//     const params = new HttpParams().set('userId', userId);
+//     return this.http.get<any[]>(`${CURRENT_API_URL}/server/api/audit/user`, { params });
+//   }
+
+// //   getAuditLogsByUser(userUuid: string): Observable<any> {
+// //   return this.http.get<any[]>(`${CURRENT_API_URL}/server/api/audit/user/${userUuid}`);
+// // }
+
+// }
